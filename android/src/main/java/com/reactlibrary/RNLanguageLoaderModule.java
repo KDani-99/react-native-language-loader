dcc92907590130d726e6fb5a7cb769f3b36170e4
KDani-99 <porsekin@gmail.com>
2020-02-03 12:38:51 +0100

    Import fix

--- android/src/main/java/com/reactlibrary/RNLanguageLoaderModule.java
+++ android/src/main/java/com/reactlibrary/RNLanguageLoaderModule.java
@@ -1,6 +1,7 @@
 
 package com.reactlibrary;
 
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
