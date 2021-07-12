a2c001bec748c504c220a2c1ffb76c1181ec0b08
KDani-99 <porsekin@gmail.com>
2021-07-12 09:13:46 +0200

    Refactor(native/wrapper): should not convert the result to json automatically

--- index.js
+++ index.js
@@ -1,153 +1,63 @@
-"use strict"
+'use strict';
 import {NativeModules} from 'react-native';
 
 const {RNLanguageLoader} = NativeModules;
 
 /**
- * Load every file from the assets/languages folder (Android), mainbundle(IOS)
- * @example
- * var loadLanguages = async () =>
- * {
- *  let languages = await LanguageLoader.loadLanguagesAsync('json');
- *  if(!languages)
- *  {
- *      // error!
- *      return;
- *  }
- *  // use `languages`
- * }
+ * Returns the content of a specific file from the assets/languages folder (Android), main bundle (IOS) with the given extension, asynchronously.
+ *
  * @async
- * @name loadLanguagesAsync
- * @typedef function
- * @param {string} extension - File extension (default: json)
- * @returns Promise
- * 
+ * @method
+ * @param {string} fileName - The name of the language file
+ * @param {string} extension - The extension of the file
+ * @returns {Promise<string>} - The content of a specific file
  * @throws IOS: `could_not_read_content` error | Android: IOException
  */
-async function loadLanguagesAsync(extension = 'json')
-{
-    if(extension === 'json')
-        return RNLanguageLoader.loadLanguagesAsync(extension)
-        .then(langs=>JSON.parse(langs))
-    else
-        return RNLanguageLoader.loadLanguagesAsync(extension);
+function loadLanguageAsync(fileName = '', extension = 'json') {
+    return RNLanguageLoader.loadLanguageAsync(fileName, extension);
 }
 
 /**
- * Load a specific file from the assets/languages folder (Android), mainbundle(IOS)
- * @example
- * var loadLanguage = async () => 
- * {
- *  let language = await LanguageLoader.loadLanguageAsync('en','json');
- *  if(!language)
- *  {
- *      // error!
- *      return;
- *  }
- *  // use `language`
- * }
+ * Returns the content of every file from the assets/languages folder (Android), main bundle (IOS) with the given extension, asynchronously.
+ *
  * @async
- * @name loadLanguageAsync
- * @typedef function
- * @param {string} fileName - Name of the file (default: '')
- * @param {string} extension - File extension (default: json)
- * @returns Promise
- * 
- * @throws IOS: `file_not_found`, `could_not_read_content` error | Android: IOException
+ * @method
+ * @param extension
+ * @returns {Promise<string[]>} - The content of every file with the given extension
+ * @throws IOS: `could_not_read_content` error | Android: IOException
  */
-async function loadLanguageAsync(fileName = '', extension = 'json')
-{
-    if(extension === 'json')
-        return NLanguageLoader.loadLanguageAsync(fileName,extension)
-        .then(lang=>JSON.parse(lang));
-    else
-        return RNLanguageLoader.loadLanguageAsync(fileName,extension);
-}
-/**
- * Load a specific file from the assets/languages folder (Android), mainbundle(IOS)
- * @example
- * LanguageLoader.loadLanguage('en','json',(error,language)=>{
- *  if(error)
- *  {   
- *      // error!
- *      return;
- *  }
- *  // use `language`
- * });
- * @callback
- * @name loadLanguage
- * @typedef function
- * @param {string} fileName - Name of the file (default: '')
- * @param {string} extension - File extension (default: json)
- * @param {string} callback - Callback(error,language) (default: null)
- * @returns Callback
- * 
- * @throws IOS: `could_not_read_content`,`fil_not_found` error | Android: IOException
- */
-function loadLanguage(fileName = '', extension = 'json',callback = null)
-{
-    if(extension === 'json')
-        return RNLanguageLoader.loadLanguage(fileName,extension,(error,language)=>
-        {
-            if(error)
-                return callback(error,null);
-            try
-            {
-                let data = JSON.parse(language);
-                callback(null,data);
-            }
-            catch(ex)
-            {
-                return callback(ex,null);
-            }
-        });
-    else
-        return RNLanguageLoader.loadLanguage(fileName,extension,callback);
-}
-/**
- * Load every file from the assets/languages folder (Android), mainbundle(IOS)
- * @example
- * LanguageLoader.loadLanguages('json',(error,languages)=>{
- *  if(error)
- *  {
- *      // error!
- *      return;
- *  }
- *  // use `languages`
- * });
- * @callback
- * @name loadLanguages
- * @typedef function
- * @param {string} extension - File extension (default: json)
- * @param {string} callback - Callback(error,languages) (default: null)
- * @returns Callback
- * 
- * @throws IOS: `could_not_load_files` error | Android: IOException
- */
-function loadLanguages(extension = 'json',callback = null)
-{
-    if(extension === 'json')
-        return RNLanguageLoader.loadLanguages(extension,(error,languages)=>
-        {
-            if(error)
-                return callback(error,null);
-            try
-            {
-                let data = JSON.parse(languages);
-                callback(null,data);
-            }
-            catch(ex)
-            {
-                return callback(ex,null);
-            }
-        });
-    return RNLanguageLoader.loadLanguages(extension,callback);
+function loadLanguagesAsync(extension = 'json') {
+    return RNLanguageLoader.loadLanguagesAsync(extension);
 }
 
-export default 
-{
+/**
+ * Returns the content of a specific file from the assets/languages folder (Android), main bundle (IOS).
+ *
+ * @method
+ * @param {string} fileName - The name of the language file
+ * @param {string} extension - The extension of the file
+ * @param callback - function, Callback(error, language) (default: null)
+ * @throws IOS: `could_not_read_content` error | Android: IOException
+ */
+function loadLanguage(fileName = '', extension = 'json', callback = null) {
+    return RNLanguageLoader.loadLanguage(fileName, extension, callback);
+}
+
+/**
+ * Returns the content of every file from the assets/languages folder (Android), main bundle(IOS)
+ *
+ * @method
+ * @param {string} extension - The extension of the language files
+ * @param callback - function, Callback(error, languages) (default: null)
+ * @throws IOS: `could_not_read_content` error | Android: IOException
+ */
+function loadLanguages(extension = 'json', callback = null) {
+    return RNLanguageLoader.loadLanguages(extension, callback);
+}
+
+export default {
     loadLanguage,
     loadLanguages,
     loadLanguageAsync,
-    loadLanguagesAsync
-}
+    loadLanguagesAsync,
+};
